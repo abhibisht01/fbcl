39f7330710594ca4f820532cfce937e3298bdf0e
Abhishek Bisht <99400134+abhibisht01@users.noreply.github.com>
2022-06-24 18:24:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAhD-QfkLQkh2OII_Jo19psXtDPwHiGR2U",
-    authDomain: "profb-cl.firebaseapp.com",
-    projectId: "profb-cl",
-    storageBucket: "profb-cl.appspot.com",
-    messagingSenderId: "178759105351",
-    appId: "1:178759105351:web:c3d6ad6e22c02925a4aa62",
-    measurementId: "G-2ZT91W45VY"
+   Copy Paste Your Config Files
 };
 
 // Initialize Firebase
